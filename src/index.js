5ce2b903e37ae47ff4a2e288ab84329c80c83fc1
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-03-22 14:10:01 -0300

    Connecting to db in mysql workbench

--- src/index.js
+++ src/index.js
@@ -3,33 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
-
 const app = express();
+const db = require("./database/models/");
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT;
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Conectado ao banco de dados");
-    app.listen(port, () =>
-      console.log(`Example app listening on port ${port}!`)
-    );
-  })
-  .catch((err) => {
-    console.log("Erro: " + err);
-  });
+db.sequelize.sync().then(() => {
+  console.log("Conectado ao banco de dados");
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
