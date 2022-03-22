741b3e2d28b5850e4f9e96005dfe2b82e3f2919a
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-03-22 13:34:52 -0300

    Connecting to local mysql workbench db

--- src/index.js
+++ src/index.js
@@ -4,13 +4,32 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
 const port = process.env.PORT;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conectado ao banco de dados");
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  })
+  .catch((err) => {
+    console.log("Erro: " + err);
+  });
