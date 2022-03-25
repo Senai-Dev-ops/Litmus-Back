f80776c01fdc6cc1ddfd8a2497cb04bb6a0bb391
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-03-25 15:30:08 -0300

    Fixing UserController

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -8,19 +8,23 @@ module.exports = {
             const userEmail = await User.findOne({ where: { email } })
             const userCpf = await User.findOne({ where: { cpf } })
 
-            bcrypt.hash(password, 10).then((hash) => {
-                User.create({ name: name, email: email, password: hash, cpf: cpf, adm: adm })
-                if (userEmail) {
-                    res.status(400).json({ message: 'Já existe um usuário com este email' })
-                    return
-                }
-                if (userCpf) {
-                    res.status(400).json({ message: 'Já existe um usuário com este cpf' })
-                    return
-                } else {
+            if (userEmail) {
+                res.status(400).json({ message: 'Já existe um usuário com este email' })
+                return
+            }
+            if (userCpf) {
+                res.status(400).json({ message: 'Já existe um usuário com este cpf' })
+                return
+            }
+            if (name === "" || email === "" || password === "" || cpf === "") {
+                res.status(400).json({ message: 'Campos não podem ser nulos' })
+                return
+            } else {
+                bcrypt.hash(password, 10).then((hash) => {
+                    User.create({ name: name, email: email, password: hash, cpf: cpf, adm: adm })
                     res.status(200).json({ message: "Usuário criado com sucesso" })
-                }
-            })
+                })
+            }
         } catch (error) {
             res.status(400).json({ error })
         }
