b7c7ff8d875cff7773b9e6415452b367594676a0
gabrielS <gabrielsantos.gsf@gmail.com>
2022-05-27 17:08:34 -0300

    Logica Ligado/Desligado

--- src/controller/MaquinaController.js
+++ src/controller/MaquinaController.js
@@ -1,4 +1,7 @@
+const CreateData =  require("../utils/data");
 const { Maquina } = require("../database/models");
+
+const createDataClass = new CreateData();
 
 module.exports = {
   async maquinaList(req, res) {
@@ -9,15 +12,21 @@ module.exports = {
         res.status(401).json({ message: "Não há informções da máquina" });
       }
       res.status(200).json({ infos });
-    } catch (error) { }
+    } catch (error) {}
   },
 
   async statusMaquina(req, res) {
     try {
-      const { status } = req.params;
+      const status = req.params;
 
-    } catch (error) {
+      if (status.status == 1) {
+        res.json({ message: "Máquina ligada!" });
+        createDataClass.start()
+      } else {
+        res.json({ message: "Máquina desligada!" });
+        createDataClass.stop()
+      }
 
-    }
+    } catch (error) {}
   },
 };
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,132 +1,53 @@
 const { Maquina } = require("../database/models");
 //lógica para gerar dados aleatorios de rotação, avanço e temperatura
 
-var sin = 0; // sinal se a máquina está ligada(1) ou desligada(0)
-if (sin == 1) {
-  let valor = Math.floor(Math.random() * (100 - 1)) + 1;
-  let valor2 = Math.floor(Math.random() * (30 - 1)) + 1;
-
-  while (valor == valor2) {
-    let valor2 = Math.floor(Math.random() * (100 - 1)) + 1;
+module.exports = class CreateData {
+  constructor() {
+    this.check = true;
   }
-  temporiza(valor, valor2);
-  function dnv(valor) {
-    if (sin == 1) {
-      let valor2 = Math.floor(Math.random() * (100 - 1)) + 1;
 
-      while (valor == valor2) {
-        let valor2 = Math.floor(Math.random() * (100 - 1)) + 1;
-      }
-      temporiza(valor, valor2);
-    }
-  }
-}
-
-function temporiza(valor, valor2) {
-  var cont = 0;
-  var cont2 = Math.floor(Math.random() * (14 - 1)) + 1;
-
-  inter = setInterval(function () {
-    cont++;
+  temporiza(valor) {
     //gravar dado no bd
-    console.log(
-      "\nRotação atual: ",
-      Math.floor(80 * valor + (Math.random() * (100 - 1) + 1))
+    const rotacao = Math.floor(80 * valor + (Math.random() * (100 - 1) + 1));
+    const avanco = Math.floor(
+      0.35 * valor + (Math.random() * (0.4375 - 0) + 0)
     );
-    console.log(
-      "\nAvanço: ",
-      Math.floor(0.35 * valor + (Math.random() * (0.4375 - 0) + 0))
-    );
-    console.log(
-      "\nTemperatura: ",
-      30 + Math.floor(0.5 * valor + (Math.random() * (6.25 - 0) + 0))
-    );
+    const temperatura =
+      30 + Math.floor(0.5 * valor + (Math.random() * (6.25 - 0) + 0));
+
+    console.log("\nRotação atual: ", rotacao);
+    console.log("\nAvanço: ", avanco);
+    console.log("\nTemperatura: ", temperatura);
     let data = new Date().toLocaleString("pt-BR", {
       timeZone: "America/Sao_Paulo",
     });
     console.log(data);
-    //console.log(data)
+
     Maquina.create({
-      rotacao: Math.floor(80 * valor + (Math.random() * (100 - 1) + 1)),
-      avanco: Math.floor(0.35 * valor + (Math.random() * (0.4375 - 0) + 0)),
-      temperatura:
-        30 + Math.floor(0.5 * valor + (Math.random() * (6.25 - 0) + 0)),
+      rotacao: rotacao,
+      avanco: avanco,
+      temperatura: temperatura,
       datahora: data.toString(),
     });
+  }
 
-    if (cont > cont2) {
-      clearInterval(inter);
-      if (valor < valor2) {
-        var pr = valor;
+  gerar() {
+    if(this.check == false) return;
 
-        var seg = valor2;
-        int = setInterval(function () {
-          pr++;
-          //gravar dado no bd
-          console.log(
-            "\nRotação atual: ",
-            Math.floor(80 * pr + (Math.random() * (1000 - 1) + 1))
-          );
-          console.log(
-            "\nAvanço: ",
-            Math.floor(0.35 * pr + (Math.random() * (4.375 - 1) + 1))
-          );
-          console.log(
-            "\nTemperatura: ",
-            30 + Math.floor(0.5 * pr + (Math.random() * (6.25 - 0) + 0))
-          );
-          let data = new Date().toLocaleString("pt-BR", {
-            timeZone: "America/Sao_Paulo",
-          });
-          Maquina.create({
-            rotacao: Math.floor(80 * pr + (Math.random() * (1000 - 1) + 1)),
-            avanco: Math.floor(0.35 * pr + (Math.random() * (4.375 - 1) + 1)),
-            temperatura:
-              30 + Math.floor(0.5 * pr + (Math.random() * (6.25 - 0) + 0)),
-            datahora: data.toString(),
-          });
-          if (pr >= seg) {
-            let valor = seg;
-            clearInterval(int);
-            dnv(valor);
-          }
-        }, 1500); //determinar em milisegundos o intervalo entre cada envio para o bd
-      } else {
-        var pr = valor2;
+    let valor = Math.floor(Math.random() * (100 - 1)) + 1;
+    this.temporiza(valor);
+  }
 
-        var seg = valor;
-        int = setInterval(function () {
-          seg--;
-          //gravar dado no bd
-          console.log(
-            "\nRotação atual: ",
-            Math.floor(80 * seg + (Math.random() * (1000 - 1) + 1))
-          );
-          console.log(
-            "\nAvanço: ",
-            Math.floor(0.35 * seg + (Math.random() * (4.375 - 1) + 1))
-          );
-          console.log(
-            "\nTemperatura: ",
-            30 + Math.floor(0.5 * seg + (Math.random() * (6.25 - 0) + 0))
-          );
-          let data = new Date().toLocaleString("pt-BR", {
-            timeZone: "America/Sao_Paulo",
-          });
-          Maquina.create({
-            rotacao: Math.floor(80 * seg + (Math.random() * (1000 - 1) + 1)),
-            avanco: Math.floor(0.35 * seg + (Math.random() * (4.375 - 1) + 1)),
-            temperatura:
-              30 + Math.floor(0.5 * seg + (Math.random() * (6.25 - 0) + 0)),
-            datahora: data.toString(),
-          });
-          if (seg <= pr) {
-            let valor = pr;
-            clearInterval(int);
-            dnv(valor);
-          }
-        }, 1500); //determinar em milisegundos o intervalo entre cada envio para o bd
-      }
+  start() {
+    this.check = true
+    while (true) {
+      inter = setInterval(() => {
+        this.gerar();
+      }, 2000);
     }
-  }, 1500); //determinar em milisegundos o intervalo entre cada envio para o bd
-}
+  }
+
+  stop() {
+    this.check = false;
+  }
+};
